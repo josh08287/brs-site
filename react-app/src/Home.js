75411d5a59499b6d0bd95eeea5f2fa356cde241e
Josh Blaine <josh@blainesrepair.com>
2025-02-24 12:13:36 -0500

    more

--- react-app/src/Home.js
+++ react-app/src/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => (
   <div className="content-container">
     <div className="content-title-group">
-      <h2 className="title">Blaine's Repair Service and Towing, Inc.</h2>
+      <h2 className="title">Blaines Repair Service and Towing, Inc.</h2>
       <p>Schedule your vehicle service!</p>
     </div>
   </div>
