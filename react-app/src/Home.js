788d0cbb28bd5ab786eb17e6ebc27cb0a5258c4b
Josh Blaine <josh@blainesrepair.com>
2025-03-30 14:06:19 -0400

    fix chat?

--- react-app/src/Home.js
+++ react-app/src/Home.js
@@ -8,6 +8,7 @@ const Home = () => (
 	  <iframe title="booking_frame" src='https://outlook.office.com/owa/calendar/BlainesRepairServiceInc@blainesrepair.com/bookings/' width='100%' height='100%' scrolling='yes' style='border:0'></iframe>
     </div>
   </div>
+  <script type="text/javascript" src="https://res.public.onecdn.static.microsoft/customerconnect/v1/7dttl/init.js" id="chatbot" environmentId="edc504bf-7d61-efc4-b524-281619104415" crossorigin="anonymous"></script>
 );
 
 export default Home;
